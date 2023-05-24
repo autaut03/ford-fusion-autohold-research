86f3742b00b73d4bb3790f247edfb8753b548629
Alex Wells <autaut03@gmail.com>
2023-05-25 00:51:09 +0300

    Remove FCIM AutoHold handling

--- sketch_may17a_copy_20230523212220/sketch_may17a_copy_20230523212220.ino
+++ sketch_may17a_copy_20230523212220/sketch_may17a_copy_20230523212220.ino
@@ -4,28 +4,20 @@
 #define BUTTON_PIN 7
 #define LED_PIN 6
 #define CAN_HS2_PIN 9
-#define CAN_MS_PIN 10
 
 #define DEBOUNCE_DURATION 150
 
 #define BRAKE_SYS_FEATURES_3_CAN_ID 1054
-#define CLIMATE_CONTROL_DATA_FD1_CAN_ID 1050
 #define ABS_DATA_CAN_ID 1073
 
 struct can_frame canIncomingMsg;
 struct can_frame canOutgoingMsg;
-struct can_frame climateControlData = {
-  CLIMATE_CONTROL_DATA_FD1_CAN_ID,
-  8,
-  {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-};
 struct can_frame absControlData = {
   ABS_DATA_CAN_ID,
   8,
   {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
 };
 MCP2515 hs2Bus(CAN_HS2_PIN);
-MCP2515 msBus(CAN_MS_PIN);
 
 byte autoHoldIndicatorState = LOW;
 byte lastButtonState = LOW;
@@ -36,8 +28,7 @@ void setup() {
   pinMode(BUTTON_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  while (!Serial)
-    ;
+  while (!Serial);
   Serial.begin(115200);
 
   SPI.begin();
@@ -54,16 +45,10 @@ void setup() {
   hs2Bus.setFilter(MCP2515::RXF4, false, ABS_DATA_CAN_ID);
   hs2Bus.setFilter(MCP2515::RXF5, false, BRAKE_SYS_FEATURES_3_CAN_ID);
   hs2Bus.setNormalMode();
-
-  msBus.reset();
-  msBus.setBitrate(CAN_125KBPS, MCP_8MHZ);
-  msBus.setNormalMode();
-  // can2.setFilter(MCP2515::RXF0, false, BRAKE_SYS_FEATURES_3_CAN_ID);
 }
 
 void loop() {
   readButtonPress();
-  readMsCanMessages();
   readHs2CanMessages();
   digitalWrite(LED_PIN, autoHoldIndicatorState);
 }
@@ -86,34 +71,8 @@ void readButtonPress() {
 }
 
 void emitButtonState(byte state) {
-  Serial.println("");
   Serial.println(String(">>>>>> Emitting button state as ") + String(state == HIGH ? "ON" : "OFF"));
-  Serial.println("");
-
-  // canOutgoingMsg.can_id = CLIMATE_CONTROL_DATA_FD1_CAN_ID;
-  // canOutgoingMsg.can_dlc = 8;
-  // canOutgoingMsg.data[0] = 0x00;
-  // canOutgoingMsg.data[1] = 0x00;
-  // canOutgoingMsg.data[2] = state == HIGH ? 0x08 : 0x00;
-  // canOutgoingMsg.data[3] = 0x00;
-  // canOutgoingMsg.data[4] = 0x00;
-  // canOutgoingMsg.data[5] = 0x00;
-  // canOutgoingMsg.data[6] = 0x00;
-  // canOutgoingMsg.data[7] = 0x00;
-
-  // msBus.sendMessage(&canOutgoingMsg);
-
-  // canOutgoingMsg.can_id = CLIMATE_CONTROL_DATA_FD1_CAN_ID;
-  // canOutgoingMsg.can_dlc = 8;
-
-  // for (int i = 0; i < 8; i++) {
-  //   canOutgoingMsg.data[i] = climateControlData.data[i];
-  // }
-
-  // bitWrite(canOutgoingMsg.data[2], 3, state == HIGH ? 1 : 0);
-
-  // msBus.sendMessage(&canOutgoingMsg);
-
+  
   if (state != HIGH) {
     return;
   }
@@ -125,33 +84,16 @@ void emitButtonState(byte state) {
     canOutgoingMsg.data[i] = absControlData.data[i];
   }
 
+  // So the way it's implemented by Ford is it sends an "AutoHold pressed" signal first,
+  // then after a short time another "AutoHold NOT pressed" signal.
+  // I've found the delay of 10ms to work reliably 99.99% of the time.
   bitWrite(canOutgoingMsg.data[7], 2, 1);
-
   hs2Bus.sendMessage(&canOutgoingMsg);
-
-  // bitWrite(canOutgoingMsg.data[0], 0, 0);
-  // bitWrite(canOutgoingMsg.data[1], 3, 0);
-  // bitWrite(canOutgoingMsg.data[4], 0, 0);
-  // bitWrite(canOutgoingMsg.data[5], 7, 0);
-  bitWrite(canOutgoingMsg.data[7], 2, 0);
 
   delay(10);
 
+  bitWrite(canOutgoingMsg.data[7], 2, 0);
   hs2Bus.sendMessage(&canOutgoingMsg);
-}
-
-void readMsCanMessages() {
-  if (msBus.readMessage(&canIncomingMsg) != MCP2515::ERROR_OK) {
-    return;
-  }
-
-  if (canIncomingMsg.can_id != CLIMATE_CONTROL_DATA_FD1_CAN_ID) {
-    return;
-  }
-
-  for (int i = 0; i < 8; i++) {
-    climateControlData.data[i] = canIncomingMsg.data[i];
-  }
 }
 
 void readHs2CanMessages() {
